94167219cd6be3a2d78cdbb27008f820d0154626
Nhat <0306181347@caothang.edu.vn>
2020-12-01 10:26:12 +0700

    Add HoaDon and ChiTietHoaDon

---
+++ DoAn_Ecommerce/Models/ChiTietHoaDon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_Ecommerce.Models
+{
+    public class ChiTietHoaDon
+    {
+        public int HoaDon { get; set; }
+        public int SanPham { get; set; }
+        public int SoLuongMua { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal ThanhTien { get; set; }
+    }
+}
---
+++ DoAn_Ecommerce/Models/HoaDon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_Ecommerce.Models
+{
+    public class HoaDon
+    {
+        public int Id { get; set; }
+        public int KhachHang { get; set; }
+        public DateTime NgayLapHD { get; set; }
+    }
+}
