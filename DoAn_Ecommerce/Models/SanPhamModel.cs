b39bfaa0c820cd08c675ab1d8f7dbdfe8b6ad766
Phi <0306181350@caothang.edu.vn>
2020-12-01 08:54:13 +0700

    add model

---
+++ DoAn_Ecommerce/Models/SanPhamModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_Ecommerce.Models
+{
+    public class SanPhamModel
+    {
+        public int Id { set; get; }
+        
+        public string TenSanPham { set; get; }
+
+        public string HinhAnh { set; get; }
+
+        public int SoLuongTon { set; get; }
+
+        public decimal Gia { set; get; }
+        public string Mota { set; get; }
+
+       public string NoiDung { set; get; }
+        public int id_LoaiSanPham { set; get; }
+        [ForeignKey("id_LoaiSanPham")]
+        public virtual LoaiSanPhamModel LoaiSanPham { set; get; }
+    }
+}
