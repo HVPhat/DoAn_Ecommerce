9acd8c7137db6a07d63257e3938f2aeebc15251a
justonlyme198 <0306181343@caothang.edu.vn>
2020-12-03 19:25:42 +0700

    add KhachHang , BinhLuan

---
+++ DoAn_Ecommerce/Models/BinhLuanModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_Ecommerce.Models
+{
+    public class BinhLuanModel
+    {
+        public int Id { get; set; }
+        public int NguoiBinhLuan { get; set; }
+        [ForeignKey("NguoiBinhLuan")]
+        public string NoiDung { get; set; }
+        public int LstSanPham { get; set; }
+        [ForeignKey("SanPham")]
+        public bool TrangThai { get; set; }
+        public virtual SanPhamModel SanPham { get; set };
+
+
+    }
+}
