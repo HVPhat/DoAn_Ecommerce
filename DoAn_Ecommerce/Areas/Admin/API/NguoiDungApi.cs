e1e61f97a5b0aa77777c56e80f1fc0684e3a5125
HVPhat <0306181349@caothang.edu.vn>
2021-01-10 22:29:45 +0700

    Tao api cap nhat trang thai nguoi dung va thong bao cap nhat bang bt modal

---
+++ DoAn_Ecommerce/Areas/Admin/API/NguoiDungApi.cs
@@ -0,0 +1,43 @@
+﻿using DoAn_Ecommerce.Areas.Admin.Data;
+using DoAn_Ecommerce.Areas.Admin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_Ecommerce.Areas.Admin.API
+{
+    [Route("Admin/api/[controller]")]
+    [ApiController]
+    public class NguoiDungApi : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public NguoiDungApi(DPContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("ChangeUserState/{id}")]
+        public async Task<ActionResult<NguoiDungModel>> ChangeUserState(int id)
+        {
+            var user = await _context.NguoiDung.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                user.TrangThai = !user.TrangThai;
+                await _context.SaveChangesAsync();
+                return user;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                    throw;
+            }
+        }
+    }
+}
