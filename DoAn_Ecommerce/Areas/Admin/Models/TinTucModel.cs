7314cb0126666a215b44c2326401f5daf9ceb0ba
= <=>
2021-01-16 07:51:24 +0700

    validation

--- DoAn_Ecommerce/Areas/Admin/Models/TinTucModel.cs
+++ DoAn_Ecommerce/Areas/Admin/Models/TinTucModel.cs
@@ -10,8 +10,14 @@ namespace DoAn_Ecommerce.Areas.Admin.Models
 	public class TinTucModel
 	{
 		public int Id { get; set; }
+		[Required]
+		[MaxLength(32), MinLength(10)]
 		public string TieuDe { get; set; }
+		[Required]
+		[MaxLength(100)]
 		public string TomTat { get; set; }
+		[Required]
+		[MaxLength(255)]
 		public string NoiDung { get; set; }
 		public DateTime NgayDang { get; set; }
 		public int LuotXem { get; set; }
